45bd44157340816271a56fb04dca1057bb076f1b
Fangzhen <xkfz007@users.noreply.github.com>
2015-10-29 14:33:50 +0800

    update

---
+++ Test/src/test_window.java
@@ -0,0 +1,42 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+public class test_window {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					test_window window = new test_window();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public test_window() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
